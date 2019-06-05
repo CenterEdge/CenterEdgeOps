83d1602af249548ffd9e3b7f83a258476ddd75ca
Ara Yapejian <ayapejian@centeredgesoftware.com>
2019-06-05 09:29:18 -0400

    pr changes (DO-525)

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,14 +1,3 @@
-# Provider
-provider "aws" {
-  region     = "us-east-1"
-
-  assume_role {
-    role_arn = "arn:aws:iam::833738481970:role/Deployer"
-  }
-
-  version = "~> 2.2"
-}
-
 # State
 terraform {
   backend "s3" {
---
+++ terraform/provider.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region     = "us-east-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::833738481970:role/Deployer"
+  }
+
+  version = "~> 2.2"
+}
