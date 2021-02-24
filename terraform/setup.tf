5b85a5aef807436de9d09cc0833fec5b3e4627c3
Ara Yapejian <ayapejian@centeredgesoftware.com>
2021-02-24 08:50:57 -0500

    Combine tf setup files (DO-1148)

--- terraform/setup.tf
+++ terraform/setup.tf
@@ -36,6 +36,16 @@ data "terraform_remote_state" "opsroot" {
   }
 }
 
-# Identity running terraform
 data "aws_caller_identity" "current" {
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::833738481970:role/Deployer"
+  }
+}
+
+provider "local" {
 }
